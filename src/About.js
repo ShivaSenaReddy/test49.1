3e5e8375b3af81864bb41065114c0c7e4b9308d8
ShivaSenaReddy <shivasenajagapathi7@gmail.com>
2022-10-25 18:12:28 +0530

    changes

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ function About() {
 	return <div>
 		<h2>GeeksforGeeks is a computer science portal for geeks!</h2>
 
-		Read more about us at :
+		Read more about us at jssr :
 		<a href="https://www.geeksforgeeks.org/about/">
 			https://www.geeksforgeeks.org/about/
 		</a>
